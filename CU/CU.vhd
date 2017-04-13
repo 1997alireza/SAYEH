a15470f85dc593b6b4b27d7f5d5ae797c870c1e3
Alireza Torabian <torabian.alireza@gmail.com>
2017-04-14 02:28:37 +0430

    Add CU processes and some states

--- CU/CU.vhd
+++ CU/CU.vhd
@@ -8,7 +8,7 @@ entity CU is
 		  MemDataReady                                             -- memory
 		        : in STD_LOGIC;
 		        
-		  IRout : out STD_LOGIC_VECTOR(15 downto 0);               -- IR 
+		  IRout : in STD_LOGIC_VECTOR(15 downto 0);                -- IR 
 		  
 		  CSet, CReset, ZSet, ZReset, SRload,                      -- flags
 		  B15to0, AandB, AorB, AxorB, notB, AcmpB, shrB,
@@ -28,7 +28,96 @@ entity CU is
 end entity;
 
 architecture CU_ARCH of CU is 
+  type state is (reset, fetch, baseExe, shadowExe, halt);
+	signal currentState : state := reset;
+	signal nextState : state;
 begin
+  
+  -- state changer
+  process (clk, ExternalReset)
+	begin
+		if ExternalReset = '1' then
+			currentState <= reset;
+		elsif clk'event and clk = '1' then
+			currentState <= nextState;
+		end if;
+	end process;
+	
+	-- control signals base on state
+ 	process (currentState)
+	begin
+	  
+	  -- set defaults
+	  CSet <= '0';
+    CReset <= '0';
+    ZSet <= '0';
+    ZReset <= '0';
+    SRload <= '0';
+    B15to0 <= '0';
+    AandB <= '0';
+    AorB <= '0';
+    AxorB <= '0';
+    notB <= '0';
+    AcmpB <= '0';
+    shrB <= '0';
+    shlB <= '0';
+    AaddB <= '0';
+    AsubB <= '0';
+    AmulB <= '0';
+    tcmpB <= '0';
+    rand <= '0';
+    Rs_on_AddressUnitRSide <= '0';
+    Rd_on_AddressUnitRSide <= '0';
+    ALUout_on_Databus <= '0';
+    Address_on_Databus <= '0';
+    WPadd <= '0';
+    WPreset <= '0';
+    IRload <= '0';
+    ResetPC <= '0';
+    EnablePC <= '0';
+    PCplusI <= '0';
+    PCplus1 <= '0';
+    RplusI <= '0';
+    Rplus0 <= '0';
+    RFLwrite <= '0';
+    RFHwrite <= '0';
+    readMem <= '0';
+    writeMem <= '0';
+    shadow <= '0';
+    
+    
+		case currentState is
+		  when halt =>
+		    nextState <= halt;
+			when reset =>
+			  CReset <= '1';
+			  ZReset <= '1';
+			  WPreset <= '1';
+			  ResetPC <= '1';
+			  EnablePC <= '1';
+			  
+  
+			  nextState <= fetch;
+			  
+			when fetch =>
+			  readMem <= '1';
+			  
+			  if(memDataReady = '1') then
+			    IRload <= '1';
+			    nextState <= baseExe;
+			  else
+			    nextState <= fetch;
+			  end if;
+			when baseExe =>
+			  
+			  
+			when shadowExe =>
+			  
+			when OTHERS =>
+			  nextState <= reset;
+			  
+		end case;
+	end process;
   
 end architecture;
 
