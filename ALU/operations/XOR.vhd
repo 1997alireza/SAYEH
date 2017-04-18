3f8635a673ad258c02eb878dccc93bb8bbd93c08
Alireza Torabian <torabian.alireza@gmail.com>
2017-04-18 23:46:22 +0430

    Implementation of XOR gate

--- ALU/operations/XOR.vhd
+++ ALU/operations/XOR.vhd
@@ -9,12 +9,33 @@ port (
 end entity;
 
 architecture xorGate_ARCH of xorGate is 
+  component xorSingleGate is
+  port(
+    input1, input2 : in STD_LOGIC;
+    output : out STD_LOGIC
+  );
+  end component;
 begin
   bitLoop : for i in 15 downto 0 generate
-    output(i) <= Rs(i) xor Rd(i);
+    xorBitI : xorSingleGate port map (Rs(i), Rd(i), output(i));
   end generate;
 end architecture;
 
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity xorSingleGate is
+port(
+  input1, input2 : in STD_LOGIC;
+  output : out STD_LOGIC
+);
+end entity;
+
+architecture xorSingleGate_ARCH of xorSingleGate is
+begin
+  output <= (input1 and not input2) or (not input1 and input2);
+end architecture;
+  
 
 
 
